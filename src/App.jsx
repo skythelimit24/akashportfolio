5625a4fe1ff90070959738b08e9d9c40590d6429
Akash <akash.tanwar101@gmail.com>
2024-03-23 20:42:40 +0530

    Navigation bar section is Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
-
+import React from "react";
+import "./app.scss"
+import Navbar from "./Navabar/Navbar";
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+    <section id="Homepage">
+      <Navbar/>
+    </section>
+    <section id="Services">Prallax</section>
+    <section>Services</section>
+    <section id="Portfolio">Prallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>portfoli03</section>
+    <section id="Contact">Contact</section>
+    </>
+  )
 };
 
 export default App;
